569e9cb7953013b68f5e9887b061f42371e7ab82
Matt Albinson <mattalbinson@berkeley.net>
2020-04-27 08:45:48 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function run() {
+
+    var artist = "drake";
+
+    $.ajax({
+        url: 'http://itunes.apple.com/search?term=' + artist,
+        dataType: "jsonp",
+        success: process
+    });
+}
+
+function process(data) {
+    console.log(data)
+
+    var songs = data.results;
+    var o = "";
+
+    for(var p=0;p<songs.length;p++) {
+        o += "<tr>";
+        o += "<td>" + songs[p].trackName + "</td>";
+        o += "<td>" + songs[p].collectionName + "</td>";
+        o += "</tr>";
+    }
+
+    var table = document.getElementById("output");
+    table.innerHTML = o;
+    table.style.display = "block";
+
+}
